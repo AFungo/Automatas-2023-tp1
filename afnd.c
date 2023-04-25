0a4b238614af182b457ba6013f58b0877e8b5404
joaquinmoran <joacomoran6@gmail.com>
2023-04-25 18:55:29 -0300

    creation of an afnd(fixed)

--- afnd.c
+++ afnd.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define MAX_STATES 10
 #define ALPHABET_SIZE 2
@@ -18,16 +19,20 @@ struct AFN{
 int main (int argc, char *argv[]){
 
 	struct AFN *a = malloc(sizeof(struct AFN));
+	int states[3] = {1,2,3};
+	int alph[2] = {0,1};
+	int finalSt[1] = {3};
+	booleanArray delta[3][3] = { //      1             2            3
+							/*1*/	{{true,false},{true,true},{false,false}},
+							/*2*/	{{false,false},{false,true},{true,true}},
+							/*3*/	{{false,true},{false,false},{true,true}}
+								}; 
 
-	a->states = {1,2,3};
-	a->alphabet = {0,1};
+	memcpy(a->states, states, sizeof(int)*3);
+	memcpy(a->alphabet, alph, sizeof(int)*2);
 	a->initialState = 1;
-	a->finalStates = {3};
-	a->delta = { //      1             2            3
-			/*1*/	{{true,false},{true,true},{false,false}},
-			/*2*/	{{false,false},{false,true},{true,true}},
-			/*3*/	{{false,true},{false,false},{true,true}}
-				};
+	memcpy(a->finalStates, finalSt, sizeof(int)*1);
+	memcpy(a->delta, delta, sizeof(int)*3*3);
 	printf("%d",a->initialState);
 
 }
