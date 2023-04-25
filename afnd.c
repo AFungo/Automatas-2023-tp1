ca08efbd8675a769be75d86797461b0d07d51022
giung1 <agus_98_jov@hotmail.com.ar>
2023-04-25 18:08:02 -0300

    delta now is a boolean array

--- afnd.c
+++ afnd.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define MAX_STATES 10
-#define ALPHABET 2
+#define ALPHABET_SIZE 2
+typedef bool booleanArray[ALPHABET_SIZE];
+
 typedef struct{
 	int states[MAX_STATES];
-	int alphabet[ALPHABET];
-	int delta[MAX_STATES][MAX_STATES][ALPHABET];
+	int alphabet[ALPHABET_SIZE];
+	booleanArray delta[MAX_STATES][MAX_STATES];
 	int initialState;
 	int finalStates[MAX_STATES];
 }automaton;
 
 
 
+int main (int argc, char *argv[]){
+	
+}
