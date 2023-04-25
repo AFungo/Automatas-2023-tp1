5742859efe6e04eaa19a46a56aca845321858e08
joaquinmoran <joacomoran6@gmail.com>
2023-04-25 18:35:02 -0300

    creation of an afnd

--- afnd.c
+++ afnd.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 #define MAX_STATES 10
 #define ALPHABET_SIZE 2
 typedef bool booleanArray[ALPHABET_SIZE];
 
-struct{
+struct AFN{
 	int states[MAX_STATES];
 	int alphabet[ALPHABET_SIZE];
 	booleanArray delta[MAX_STATES][MAX_STATES];
 	int initialState;
 	int finalStates[MAX_STATES];
-}AFN;
-
-
+};
 
 
 int main (int argc, char *argv[]){
-	
+
+	struct AFN *a = malloc(sizeof(struct AFN));
+
+	a->states = {1,2,3};
+	a->alphabet = {0,1};
+	a->initialState = 1;
+	a->finalStates = {3};
+	a->delta = { //      1             2            3
+			/*1*/	{{true,false},{true,true},{false,false}},
+			/*2*/	{{false,false},{false,true},{true,true}},
+			/*3*/	{{false,true},{false,false},{true,true}}
+				};
+	printf("%d",a->initialState);
+
 }
 
 
