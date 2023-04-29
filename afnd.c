052e8f7c428005f5f34a17fdcd6a8244bf3db90e
giung1 <agus_98_jov@hotmail.com.ar>
2023-04-28 21:08:09 -0300

    starting 'pertenece'

--- afnd.c
+++ afnd.c
@@ -15,6 +15,26 @@ struct AFN{
 	int finalStates[MAX_STATES];
 };
 
+bool pertence(struct AFN t, char *chain){
+	printf("%d \n",strlen(chain));
+	int i=0;
+	bool alph=true;
+	int k;
+	while (i < strlen(chain) && alph){
+		printf("alph is %d \n",alph);
+		alph=false;
+		k=0;
+		while (k<ALPHABET_SIZE && !alph){
+			printf("chain en 'i' is %d and alph in 'k' is %d \n",(int)chain[i],t.alphabet[k]);
+			if((int)chain[i]==t.alphabet[k]){ //esta comparando el codigo ascii de chain[i] con el caracter de t.alphabet, por ejemplo 49, donde 49 es el ascii de "1", con "1"
+				alph=true;
+			}
+		k++;
+		}
+	i++;		
+	}
+	return alph;
+}
 
 int main (int argc, char *argv[]){
 
@@ -33,12 +53,17 @@ int main (int argc, char *argv[]){
 	a->initialState = 1;
 	memcpy(a->finalStates, finalSt, sizeof(int)*1);
 	memcpy(a->delta, delta, sizeof(int)*3*3);
-	printf("%d",a->initialState);
-
+	printf("%d \n",a->initialState);
+	char string[] = "100101\0";
+	bool e=pertence(*a,string);
+	
+	printf("%d",e);
 }
 
 
-//bool pertence(AFN a, int[] chain);
+
+
+
 //void entrada(String fileName);
 //void salida(String fileName);        
 //AFD AFNtoAFD(AFN afn)
