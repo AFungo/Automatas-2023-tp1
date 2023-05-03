6044b933955755eaa002e6114d7288059fae538c
joaquinmoran <joacomoran6@gmail.com>
2023-05-03 10:39:28 -0300

    creation of struct AFD

--- afnd.h
+++ afnd.h
@@ -13,6 +13,14 @@ typedef struct{
 	int finalStates[MAX_STATES];
 }AFN;
 
+struct AFD{
+	int states[MAX_STATES];
+	int alphabet[ALPHABET_SIZE];
+	int delta[MAX_STATES][ALPHABET_SIZE]; // int o bool?
+	stateArray initialState;
+	stateArray finalState[MAX_STATES];
+};
+
 AFN readAutomaton(char *fileName);
 void automatonToString(AFN automaton);
 AFN initAutomaton(AFN automaton);
