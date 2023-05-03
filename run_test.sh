b6133a43a66e184e672a427581381dd0dc2c4bf5
giung1 <agus_98_jov@hotmail.com.ar>
2023-05-02 22:04:03 -0300

    'pertenece' almost done, got a bug when the input string of char is only one char

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,7 @@
 gcc -o test1 testCreateAutomaton.c afnd.c
-gcc -o test2 testReadBasicAutomaton.c afnd.c
-gcc -o test3 testWriteBasicAutomaton.c afnd.c
+#gcc -o test2 testReadBasicAutomaton.c afnd.c
+#gcc -o test3 testWriteBasicAutomaton.c afnd.c
 
 ./test1
-./test2
-./test3
+#./test2
+#./test3
