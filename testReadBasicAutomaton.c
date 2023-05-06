c3fd6516be5b2acf92695dd8154fee0c5669bb56
AFungo <augustofungo@gmail.com>
2023-05-06 20:43:46 -0300

    modify test with new structure and auxiliar methods

--- testReadBasicAutomaton.c
+++ testReadBasicAutomaton.c
@@ -1,5 +1,5 @@
 #include "afnd.h"
-
+#include "utils.h"
 int main(){
     AFN automaton = readAutomaton("automata.dot");
 	automatonToString(automaton);
--- testWriteAutomatonWLambda.c
+++ testWriteAutomatonWLambda.c
@@ -3,6 +3,7 @@
 int main(){
     AFN automaton = readAutomaton("automataND.dot");
 	automatonToString(automaton);
+	writeAutomaton("test_automataND.dot", automaton);
 }
 
 
