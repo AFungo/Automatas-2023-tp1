0266b2361a73620f718486fd63fed12acbe0b407
AFungo <augustofungo@gmail.com>
2023-05-08 21:21:17 -0300

    complete all test suite

--- run_test.sh
+++ run_test.sh
@@ -1,11 +1,13 @@
-gcc -o test1 testCreateAutomaton.c afnd.c
-#gcc -o test2 testReadBasicAutomaton.c afnd.c
-#gcc -o test3 testWriteBasicAutomaton.c afnd.c
-#gcc -o test4 testReadAutomatonWLambda.c afnd.c
-#gcc -o test5 testWriteAutomatonWLambda.c afnd.c
+gcc -o test1 testStringBelongTheLanguage.c afnd.c utils.c
+gcc -o test2 testReadBasicAutomaton.c afnd.c utils.c
+gcc -o test3 testWriteBasicAutomaton.c afnd.c utils.c
+gcc -o test4 testReadAutomatonWLambda.c afnd.c utils.c
+gcc -o test5 testWriteAutomatonWLambda.c afnd.c utils.c
+gcc -o test6 testTransformAFNtoAFD.c afnd.c utils.c
 
 ./test1
-#./test2
-#./test3
-#./test4
-#./test5
+./test2
+./test3
+./test4
+./test5
+./test6
